fb0339e5da2ce14b425a09f249c31ef7ccddf6af
llzllx <llzllx70@163.com>
2025-05-19 13:50:09 +0800

    modify

--- tests/lsj/local_infer.py
+++ tests/lsj/local_infer.py
@@ -1,4 +1,6 @@
 
+import os
+import json
 import time
 import base64
 from openai import OpenAI
@@ -10,84 +12,112 @@ client = OpenAI(
     base_url=openai_api_base,
 )
 
-# image_path = "testmarkdown.png"
-# image_path = "shouju.png"
-# image_path = "python.png"
-# image_path = "3.jpg"
-# image_path = 'table.png'
-# image_path = 'paper_2_column.png'
-# image_path = "008.png"
-# image_path = "009.png"
-# image_path = "选区_011.png"
-# image_path = "选区_018.png"
-# image_path = "page_1.png"
-# image_path = "page_44.png"
-# image_path = "选区_44.png"
-# image_path = "page_8.png"
-# image_path = "page_1.png"
+PROMPT = """
+图片是电梯各个部件的照片，目前有如下类别:
+{types}
+请识别当前图片属于哪个类别，只输出类别名称，不要输出其他信息。
+"""
 
 
+class XioLift:
 
-def f(image_path, model):
+    def __init__(self, img_dir):
 
-    print(f'----------------{image_path}------------------{model}-------------------------------------')
+        self.model = "Qwen2.5-VL-7B-Instruct"
+        self.root_img_path = self.build_root_img_path(img_dir)
+        self.structure = self.build_structure()
+        self.prompt = self.build_prompt()
 
-    with open(image_path, "rb") as f:
-        encoded_image = base64.b64encode(f.read())
+    def build_root_img_path(self, img_dir):
 
-    encoded_image_text = encoded_image.decode("utf-8")
-    base64_qwen = f"data:image;base64,{encoded_image_text}"
-    # print(base64_qwen)
+        # 替换为你的实际路径
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(current_dir, img_dir)
 
-    a = time.time()
-    chat_response = client.chat.completions.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {
-                "role": "user",
-                "content": [
-                    { "type": "image_url", "image_url": { "url": base64_qwen } },
-                    # {"type": "text", "text": "Extract all content from the image in detail and organize it into a more understandable format. The requirements are as follows: 1. If it's a table, convert it into Markdown table format. 2. If there are original numbered titles, retain the numbering. 3. Follow the original order of the text. 4. Do not repeatedly output characters like |."}
-                    {"type": "text", "text": "Extract all content from the image in detail and organize it into a more understandable format. The requirements are as follows: 1. If there are original numbered titles, retain the numbering. 2. Follow the original order of the text. 3. Do not repeatedly output characters like |."}
-                ]
-            }
-        ],
+    def build_prompt(self):
 
-        temperature = 0.01,
-        top_p = 0.1
-    )
+        types = str(list(self.structure.keys()))
+        return PROMPT.format(types=types)
+
+    def dir_to_dict(self, path):
+        result = {}
+        for item in os.listdir(path):
+            full_path = os.path.join(path, item)
+            if os.path.isdir(full_path):
+                result[item] = self.dir_to_dict(full_path)
+            else:
+                # 当前目录就是叶子目录，直接用目录名映射文件名列表
+                return sorted(os.listdir(path))
+
+        return result
+
+    def build_structure(self):
+
+        structure = self.dir_to_dict(self.root_img_path)
+        print(json.dumps(structure, indent=2, ensure_ascii=False))
+
+        return structure
+
+    def f(self, image_path):
+
+        # print(f'----------------{image_path}------------------{self.model}-------------------------------------')
+
+        with open(image_path, "rb") as f:
+            encoded_image = base64.b64encode(f.read())
+
+        encoded_image_text = encoded_image.decode("utf-8")
+        base64_qwen = f"data:image;base64,{encoded_image_text}"
+        # print(base64_qwen)
+
+        a = time.time()
+        chat_response = client.chat.completions.create(
+            model=self.model,
+            messages=[
+                # {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "system", "content": "你是一个分类器."},
+                {
+                    "role": "user",
+                    "content": [
+                        { "type": "image_url", "image_url": { "url": base64_qwen } },
+                        {"type": "text", "text": self.prompt}
+                    ]
+                }
+            ],
+
+            temperature = 0.01,
+            top_p = 0.1
+        )
 
 
-    r = chat_response.choices[0].message.content
-    print("Chat response:\n", r)
+        r = chat_response.choices[0].message.content
+        print("Chat response: ", r)
 
-    b = time.time()
+        b = time.time()
 
-    return f'{image_path} {model} time: {b-a}, out len: {len(r)}, ratio: {len(r)/(b-a)}'
+        # print(f'{image_path} {self.model} time: {b-a}, out len: {len(r)}, ratio: {len(r)/(b-a)}')
 
-images = [
-    "page_6.png"
-    # "page_44.png",
-    # "选区_44.png",
-    # "page_8.png",
-    # "选区_8.png",
-    # "page_1.png",
-    # "选区_1.png"
-]
+        return r
 
-ret = []
+    def run(self):
 
-# model="Qwen2-VL-7B-Instruct-AWQ"
-# model="Qwen2-VL-7B-Instruct"
-# model='Qwen2-VL-7B-Instruct-GPTQ-Int4'
-model = "Qwen2.5-VL-7B-Instruct"
+        ok = 0
+        err = 0
+
+        for type_, images in self.structure.items():
+
+            for image in images:
+                path_ = os.path.join(self.root_img_path, type_, image)
+                r = self.f(path_)
+
+                if r == type_:
+                    ok += 1
+                    print(f'ok: {ok}')
+
+                else:
+                    err += 1
+                    print(f'err: {err}, {r} != {type_}')
 
 
-for image_path in images:
+xiolift = XioLift('xiolift_img')
+xiolift.run()
 
-    r = f(image_path, model)
-    ret.append(r)
-
-for i in ret:
-    print(i)
